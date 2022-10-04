5916381706a3a3cde0a0dc0ef54f97efd57b9e4d
nazaroffart <nazaroffart@gmail.com>
2022-10-04 08:58:15 +0300

    3 update

--- 3dz/problem_3.py
+++ 3dz/problem_3.py
@@ -1,6 +1,6 @@
 i = float(input("введи число: "))
 
-if i > 0 and i < 21 and i > 57 and i < 100:
+if i > 0 and i < 21 or i > 57 and i < 100:
     print("число разрешено")
 else:
     print("число запрещено")
