0e3806295f48bcf8bc4664ed398175fbc25ab61a
nazaroffart <nazaroffart@gmail.com>
2022-10-05 13:41:41 +0300

    +auth

---
+++ 4dz/auth.py
@@ -0,0 +1,10 @@
+login = str(input("Введите пожалуйста логин: "))
+passwd = str(input("Введите пожалуйста пароль: "))
+
+if login == "art":
+    if passwd == "123456":
+        print("True")
+else:
+    print("False")
+
+
