09f34050784c8568921bdc8001c4edc9e37183cc
nazaroffart <nazaroffart@gmail.com>
2022-10-07 08:16:03 +0300

    day6

---
+++ 6dz/problem_12.py
@@ -0,0 +1,8 @@
+list1 = [
+    ("hi", 'how','are','u'),
+    ('sho','ce','tam'),
+    ('ta', 'norm','nishego','tak'),
+    ('sam', 'kak', 'sho'),
+    ('ti', 'sho', 'tam')
+        ]
+print(list1)
---
+++ 6dz/problem_72.py
@@ -0,0 +1,3 @@
+from problem_17 import NAMES
+NAMES.pop([1:10:2])
+print(NAMES)
