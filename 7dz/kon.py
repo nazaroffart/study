ea64754c2d439239018153b7132384332006e30f
nazaroffart <nazaroffart@gmail.com>
2022-10-09 14:39:16 +0300

    upd sl

--- 7dz/kon.py
+++ 7dz/kon.py
@@ -4,11 +4,11 @@ y_kon = int(input("Введи значение коня по оси у: "))
 x_fig = int(input("Введи значение фигуры по оси х: "))
 y_fig = int(input("Введи значение фигуры по оси у: "))
 
-if (x_kon + 3 == x_fig and y_kon + 1 == y_fig or 
-x_kon + 3 == x_fig and y_kon - 1 == y_fig or x_kon - 3 == x_fig and y_kon + 1 == y_fig or 
-x_kon - 3 == x_fig and y_kon - 1 == y_fig or x_kon + 1 == x_fig and y_kon + 3 == y_fig or 
-x_kon + 1 == x_fig and y_kon - 3 == y_fig or x_kon - 1 == x_fig and y_kon + 3 == y_fig or 
-x_kon - 1 == x_fig and y_kon - 3 == y_fig):
+if (x_kon + 2 == x_fig and y_kon + 1 == y_fig or 
+x_kon + 2 == x_fig and y_kon - 1 == y_fig or x_kon - 2 == x_fig and y_kon + 1 == y_fig or 
+x_kon - 2 == x_fig and y_kon - 1 == y_fig or x_kon + 1 == x_fig and y_kon + 2 == y_fig or 
+x_kon + 1 == x_fig and y_kon - 2 == y_fig or x_kon - 1 == x_fig and y_kon + 2 == y_fig or 
+x_kon - 1 == x_fig and y_kon - 2 == y_fig):
     print("Конь бьет фигуру")
 
 else:
