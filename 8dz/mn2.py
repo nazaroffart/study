158913dd92839f401749ee27ee86bde0a57aa964
nazaroffart <nazaroffart@gmail.com>
2022-10-10 13:25:03 +0300

    +hm from tlg

---
+++ 8dz/mn2.py
@@ -0,0 +1,4 @@
+names_a = {"Nicholas", "Michelle", "John", "Mercy"}
+
+print(names_a.pop())
+print(names_a)
---
+++ 8dz/sl3.py
@@ -0,0 +1,10 @@
+keys = ['red', 'green', 'blue']
+values = ['#FF0000','#008000', '#0000FF']
+
+dic = {
+        keys[0]: values[0],
+        keys[1]: values[1],
+        keys[2]: values[2]
+        }
+
+print(dic)
