42cb9c471fc3d6696fe07dfee6fe23de54e1a3c1
nazaroffart <nazaroffart@gmail.com>
2022-10-13 17:58:11 +0300

    +

--- 10dz/z6.py
+++ 10dz/z6.py
@@ -1,11 +1,12 @@
 values = ("Razakov 32", 10, 1005, ["tables", "chairs"], 23.00)
 try:
     at = 10
-    in = 15
+    iy = 15
     wo = 20
     for e in range(-at, at):
+        print(e)
         print(wo / e)
-    if at > '5':
-        print("at > 5)
-except SyntaxError:
-    print("SyntaxError")
+    if at > 5:
+        print(at > 5)
+except ZeroDivisionError:
+    print("училка в школе говорила, а ты невнимательный(-ая).\nна ноль делить низззя")
